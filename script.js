81882c025a30ad6508cd6a57fe20de71e4da7b65
gaiablade <cgandeg@gmail.com>
2021-04-08 09:56:34 -0400

    Updates

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 const https = require("https")
 const fs = require("fs");
-const request = require("request")
+const request = require("request");
 
-function b(file) {
-    //console.log(`https://www.mariowiki.com${file}`)
+function get_html(file) {
     return new Promise((resolve, reject) => {
-        request(`https://www.mariowiki.com${file}`, (error, response, body) => {
+        request(`${file}`, (error, response, body) => {
             if (error)  {
                 reject(error)
             }
@@ -19,20 +18,48 @@ function b(file) {
     })
 }
 
-async function f() {
-    console.log("Start")
-    let arr = await JSON.parse(fs.readFileSync("j.json"))
-    let shortened = [arr[0], arr[1], arr[2]]
-    console.log(shortened)
-
-    let urls = shortened.map(async (file) => {
-        try {
-            let a = await b(file)
-            return a;
-        } catch {
-            return "e"
+async function get_html_async(file) {
+    request(`${file}`, (error, response, body) => {
+        if (error)  {
+            throw new Error(error)
         }
-    });
+        else if (response && response.statusCode != 200) {
+            throw new Error("invalid status code")
+        }
+        else {
+            return body
+        }
+    })
+
 }
 
-f();
+let arr = JSON.parse(fs.readFileSync("j.json"))
+
+let urls = arr.map((file) => {
+    return get_html(`https://www.mariowiki.com${file}`)
+})
+
+Promise.all(urls).then(value => {
+    const pattern = /a href=\"(https:\/\/mario.wiki.gallery\/images\/[^\"]+)\"/
+    let a = []
+
+    console.log(`Length: ${value.length}`)
+
+    for (let html of value) {
+        let found = pattern.exec(html)
+        if (found) {
+            console.log(found[1])
+            a.push(found[1])
+        }
+    }
+
+    return a
+
+}).then(value => {
+    console.log(value)
+    fs.writeFileSync("test.json", JSON.stringify(value, null, 2))
+}).catch(err => {
+    console.log("HERE")
+    console.log(err)
+    (() => {return true})();
+})
--- source.py
+++ source.py
@@ -4,14 +4,34 @@ import json
 import ctypes
 import pathlib
 import re
-import logging
 import json
-
-logging.basicConfig(filename="log.txt", level=logging.INFO)
+import logging
 
 REGEX = r"(?i)\b((?:https?://|\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))"
 
-responses = [
+logging.basicConfig(filename="logging.txt", level=logging.DEBUG)
+
+# Log to file and console:
+def log(str):
+    with open("log.txt", "a") as file:
+        file.write(str)
+    print(str)
+
+help_string = '''
+```
+roll-bot commands:
+
+!rolld[n] -> Roll an n-sided dice.
+
+!mario -> Fun Mario picture.
+
+------------------------------------
+Event words:
+among, amogus, loss
+```
+'''
+
+RESPONSES = [
     "cringe",
     "oh brother this STINKS",
     "damn bro, that's kinda cringe",
@@ -21,72 +41,108 @@ responses = [
     "cringe detected, deploying tear gas"
 ]
 
-images = [
+IMAGES = [
     "bottle_cat.jpg",
     "new_jersey.jpg"
 ]
 
-mario_images = [] # imported from a json
+mario_images = None # imported from a json
 with open("mario_images.json") as file:
     mario_images = json.load(file)
 
-print(random.choice(responses) + " " + random.choice(images))
-
+# Load C++ library
 path = pathlib.Path().absolute() / "libroll.so"
 libRoll = ctypes.CDLL(path)
 
+# Open bot token
 token = None
 with open("token.json") as json_file:
     data = json.load(json_file)
     token = data["token"]
 
-print('initializing client')
 client = None
 try:
     client = discord.Client()
 except:
-    print('exception')
+    log("Failed to initialize client")
 
 def roll(n: int):
     r = libRoll.roll(n)
     return r
 
+
+
+
+# EVENTS:
+
+
+
 @client.event
 async def on_ready():
-    print("Ready!")
+    print("Bot connected")
 
 @client.event
 async def on_message(message):
-    print(message.author.name)
-    logging.info(f'Message from {message.author.name}: {message.content}')
-    to_chastize = "Willybold_Plack"
-    #to_chastize = "gaia_blade"
-    if message.author.name == to_chastize:
-        await message.add_reaction("💩")
-        print('here')
-        if message.attachments or re.search(REGEX, message.content):
-            mess, image = random.choice(responses), random.choice(images)
-            print(f'{mess} {image}')
-            await message.reply(mess, file=discord.File(image))
+    if "dice-roll" not in message.author.display_name:
+        log(f'''
+-New Message-
+Content: {message.content}
+Display Name: {message.author.display_name}
 
-    if message.content.startswith("!rolld"):
-        r = re.search("^!rolld(\w+).*$", message.content)
-        try:
-            n = int(r[1])
-            await message.channel.send("{0}!".format(roll(n)))
-        except ValueError:
-            await message.channel.send("Failed to parse")
+            ''')
+        to_chastize = "Willybold_Plack"
+        #to_chastize = "gaia_blade"
+        if message.author.display_name == to_chastize:
+            try:
+                await message.add_reaction("💩")
+                if message.attachments or re.search(REGEX, message.content):
+                    mess, image = random.choice(responses), random.choice(images)
+                    await message.reply(mess, file=discord.File(image))
+            except:
+                pass
 
-    if "among" in message.content.lower() or "amogus" in message.content.lower():
-        await message.reply("WHEN THE IMPOSTER IS SUS", file=discord.File("amongsomebitches.jpg"))
+        if message.content.startswith("!rolld"):
+            r = re.search("^!rolld(\w+).*$", message.content)
+            try:
+                n = int(r[1])
+                await message.channel.send("{0}!".format(roll(n)))
+            except ValueError:
+                await message.channel.send("Failed to parse")
+            except:
+                pass
 
-    if "loss" in message.content.lower():
-        await message.reply("| || || |_", file=discord.File("loss.jpg"))
+        if "among" in message.content.lower() or "amogus" in message.content.lower():
+            try:
+                await message.reply("WHEN THE IMPOSTER IS SUS", file=discord.File("amongsomebitches.jpg"))
+            except:
+                pass
 
-    if message.content == "!mario":
-        image = random.choice(mario_images)
-        print(f'mario image: {image}')
-        await message.reply("", file=discord.File(f'mario/{image}'))
+        if "loss" in message.content.lower():
+            try:
+                await message.reply("| || || |_", file=discord.File("loss.jpg"))
+            except:
+                pass
+
+        if "!mario" in message.content.lower():
+            try:
+                if message.author.display_name == to_chastize:
+                    await message.reply("", file=discord.File('amongsomebitches.jpg'))
+                else:
+                    image = random.choice(mario_images)
+                    log(f'''
+        -Sending Reply-
+        Image file: mario/{image}
+
+                ''')
+                    await message.reply("", file=discord.File(f'mario/{image}'))
+            except:
+                pass
+        
+        if "!rbhelp" in message.content.lower():
+            try:
+                await message.reply(help_string)
+            except:
+                pass
 
 
 """
@@ -97,6 +153,6 @@ async def on_error(event, *args, **kwargs):
 
 @client.event
 async def on_connect():
-    print('connected')
+    pass
 
 client.run(token)
